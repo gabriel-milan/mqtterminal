6c362491cdb165dfaec40c25a350a1ff2d1500ad
Gabriel Gazola Milan <gabriel.gazola@poli.ufrj.br>
2021-02-22 17:07:19 -0300

    Split client and server in two repositories and add CD

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# mqtterminal
+# MQTTerminal
+
+[![Build Status](https://travis-ci.com/gabriel-milan/mqtterminal.svg?branch=master)](https://travis-ci.com/gabriel-milan/mqtterminal)
+
 Run shell commands asynchronously over MQTT with very low setup
---
+++ ci/script.sh
@@ -0,0 +1,19 @@
+# This script takes care of testing your crate
+
+set -ex
+
+main() {
+    cross build --target $TARGET
+    cross build --target $TARGET --release
+
+    if [ ! -z $DISABLE_TESTS ]; then
+        return
+    fi
+
+    cross clippy -- -D warnings
+}
+
+# we don't run the "test phase" when doing deploys
+if [ -z $TRAVIS_TAG ]; then
+    main
+fi
